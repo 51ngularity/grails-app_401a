198b2e375501058720f1b49166843da683bad242
51ngularity <snenasch@gmail.com>
2021-01-31 14:52:07 +0100

    Remove field "age" from "user" class
    due to not being able to make it work
    add constrain to "birthday" instead
    add explanation-comments to constraints

--- grails-app/domain/app_401a/User.groovy
+++ grails-app/domain/app_401a/User.groovy
@@ -6,19 +6,21 @@ class User {
     String email
     String password
     Date birthday
-    private Integer age = Period.between(birthday.toInstant().atZone(ZoneId.systemDefault()).toLocalDate(), LocalDate.now() ).years
+    //private Integer age = Period.between(birthday.toInstant().atZone(ZoneId.systemDefault()).toLocalDate(), LocalDate.now() ).years
 
     static constraints = {
         username size: 3..20, unique: true, validator: { name -> name.matches(/\w+/)
         }
         email size: 3..255, email: true, unique: true
         password size: 10..50, validator: { pwd ->
-            (pwd.find(/.\p{Lu}/)
-                    && pwd.find(/\p{Ll}/)
-                    && pwd.find(/\p{N}/)
-                    && pwd.find(/[\p{P}\p{S}]/))
+            (pwd.find(/.\p{Lu}/)                    // uppercase letter
+                    && pwd.find(/\p{Ll}/)           // lowercase letter
+                    && pwd.find(/\p{N}/)            // number
+                    && pwd.find(/[\p{P}\p{S}]/))    // Punctuation or Symbol
             //&& pwd.length() in 10..50
         }
-        age range: 3..120
+        birthday validator: { birthday ->           // age between 3 and 120
+            Period.between(birthday.toInstant().atZone(ZoneId.systemDefault()).toLocalDate(), LocalDate.now() ).years in 3..120
+        }
     }
 }
